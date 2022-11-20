12bdee935f3ba5da144857a8f646e9e5349bfdfe
zyx <casper45869@sina.com>
2022-11-20 16:06:27 +0800

    初始化项目结构

---
+++ src/components/homepage/Home.js
@@ -0,0 +1,30 @@
+import React from "react";
+import "./Home.css";
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src="Octocat.png" className="App-logo" alt="logo" />
+          <p>
+            GitHub Codespaces <span className="heart">♥️</span> React
+          </p>
+          <p className="small">
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <p>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn Re
+            </a>
+          </p>
+        </header>
+      </div>
+    );
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import ReactDOM from "react-dom";
+import { BrowserRouter, Routes, Route, Navigate} from "react-router-dom";
+import Home from "./components/homepage/Home"
+import Header from "./components/header/Header"
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function Index() {
+  return (
+    <BrowserRouter>
+        <Header></Header>
+        <Routes>
+          <Route path="*" element={<Navigate to="/home"/>}></Route>
+          <Route path='/home' element={<Home />} />
+        </Routes>
+    </BrowserRouter>
+  )
+}
+
+
+ReactDOM.render(<Index />, document.getElementById("root"));
+
 
 
