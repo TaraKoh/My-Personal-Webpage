0270581eb083d07eb49795c67cecc6129a098279
zyx <casper45869@sina.com>
2022-11-26 10:42:53 +0800

    修改className

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -11,12 +11,12 @@ export default class Header extends React.Component {
                     <h3>| Welcome to my personal webpage!</h3>
                 </div>
                 <div className="HeaderContainer">
-                    <i class="bi bi-github" role="img" aria-label="GitHub"></i>
-                    <i class="bi bi-sina-weibo"></i>
-                    <i class="bi bi-instagram"></i>
-                    <i class="bi bi-r-square-fill"></i>
-                    <i class="bi bi-envelope"></i>
-                    <i class="bi bi-telephone"></i>
+                    <i className="bi bi-github" role="img" aria-label="GitHub"></i>
+                    <i className="bi bi-sina-weibo"></i>
+                    <i className="bi bi-instagram"></i>
+                    <i className="bi bi-r-square-fill"></i>
+                    <i className="bi bi-envelope"></i>
+                    <i className="bi bi-telephone"></i>
                 </div>
             </div>
         )
